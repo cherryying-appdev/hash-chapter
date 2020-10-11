9c15af8cbf78755998d28e4a9a37a0fcda301599
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-11 20:15:08 +0000

    working on find_values

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,5 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+integer = gets.chomp.to_i
+p sample_hash.values
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+person = gets.chomp.split
+person_hash = Hash.new
+person_hash.store(:name, person[0])
+person_hash.store(:age, person[1])
+person_hash.store(:occupation, person[2])
+p person_hash
+# p {:name => person[0], :age => person[1], :occupation => person[2]}
